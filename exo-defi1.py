e4a61411f7bfd43b37b4f566e67999cac0a23d31
naim4906 <hajaboudanaim@gmail.com>
2025-09-30 09:05:26 +0200

    Add files via upload

---
+++ exo-defi1.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import  math as _math
+
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16])
+plt.ylabel('some numbers')
+plt.
+
+
+def f_carre(nb):
+    return (nb * nb)
+
+def racine_carre(nb): 
+    return (_math.sqrt(nb))
+
+def val_absolut(nb):
+    if nb <
+    return( )
